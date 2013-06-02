b28588afaf2ed1acebaba9d5258d524c69e6c699
mecod <mecod@msn.com>
2013-06-02 21:44:47 +0800

    clock()是CPU占用时间，使用getchar不占用CPU时间。 改用for循环，来消耗CPU时间。

--- route_4_6_clock.c
+++ route_4_6_clock.c
@@ -4,13 +4,22 @@
 int main(void)
 {
     clock_t start, end;
+    int i, j;
+    double x;
+    
     start = clock();
-    printf("Clocks at start %f.\n", (double)start);
-
+    printf("Clocks at start %lu.\n", (unsigned long)start);
+#if 0
     printf("Please wait a few seconds and then press ENTER/RETURN key");
     fflush(stdout);
 
     getchar();
+#endif
+    for (j = 0; j < 10; j++) {
+    for (i = 0; i < 100000000; i++) {
+        x = 3.45 *( 5.26 + 1);
+	}
+	}
     /* the code you want timed goes here */
     end = clock();
     printf("Clocks at end %f.\n", (double)end);
